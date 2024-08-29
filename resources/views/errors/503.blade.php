fc908cbff15759f445790cf22a2165f8a53b0c3f
eserranda <eser.randa@gmail.com>
2024-08-29 08:07:37 +0800

    add view error 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Under Maintenance</title>
+</head>
+
+<body>
+    <h1>Upsss.... Sepertinya ada yang belum beres.</h1>
+    <p>Maaf atas ketidaknyamanan ini :( Kami akan segera kembali!</p>
+</body>
+
+</html>
